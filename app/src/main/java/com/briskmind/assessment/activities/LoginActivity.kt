16cb9f7c650d046d6035e8d89eb9f210be19871a
shivamsinghal71 <80383000+shivamsinghal71@users.noreply.github.com>
2023-08-15 15:26:28 +0530

    add string

--- app/src/main/java/com/briskmind/assessment/activities/LoginActivity.kt
+++ app/src/main/java/com/briskmind/assessment/activities/LoginActivity.kt
@@ -1,19 +1,8 @@
 package com.briskmind.assessment.activities
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.graphics.Rect
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.view.Window
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.briskmind.assessment.R
 import dagger.hilt.android.AndroidEntryPoint
 
